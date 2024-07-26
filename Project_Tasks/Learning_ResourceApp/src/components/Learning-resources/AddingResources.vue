92a1e7e6a58ffe5a430d387e4efbfca14a0e19f0
Akash-gitRep <akashvijayakumar@devaxcel.com>
2024-07-26 18:24:08 +0530

    Add files via upload

---
+++ Project_Tasks/Learning_ResourceApp/src/components/Learning-resources/AddingResources.vue
@@ -0,0 +1,82 @@
+<template>
+   <base-card>
+   <form action="" @submit.prevent="submitData">
+    <div class="form-contact">
+    <label for="title">Title</label>
+    <input type="text" id="title" label="title" ref="titleIn">
+    </div>
+    <div class="form-contact">
+    <label for="description">Description</label>
+   <textarea name="" id="description" cols="30" rows="5" ref="descIn"></textarea>
+    </div>
+    <div class="form-contact">
+    <label for="link">Url-link</label>
+    <input type="url" id="link" label="link" ref="urlIn">
+    </div>
+    <div id="lastButton">
+       <base-button type="submit">Add Resource</base-button>
+    </div>
+   </form>
+   </base-card>
+</template>
+
+
+<script>
+export default {
+
+    inject:['addResource'],
+
+    
+
+    
+    methods:{
+        submitData(){
+
+            const enteredTitle=this.$refs.titleIn.value;
+            const enteredDescription=this.$refs.descIn.value;
+            const enteredLink=this.$refs.urlIn.value;
+
+            this.addResource(enteredTitle,enteredDescription,enteredLink);
+
+            if(enteredTitle.trim()===''|| enteredDescription.trim()==='' || enteredLink.trim()===''){
+                return;
+            }
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+label {
+    margin: 20px 0px 0px 0px;
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+#lastButton{
+    margin: 20px 0px 0px 0px;
+
+}
+</style>
---
+++ Project_Tasks/Learning_ResourceApp/src/components/Learning-resources/TheResources.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <base-card>
+      <base-button @click="setSelectedTab('StoredResources')" :mode="storedResButtonMode">Stored Resource</base-button>
+      <base-button @click="setSelectedTab('AddingResources')" :mode="addResButtonMode">Add Resource</base-button>
+    </base-card>
+    <keep-alive>
+      <component :is="selectedTab"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddingResources from './AddingResources.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddingResources
+  },
+  data() {
+    return {
+      selectedTab: 'StoredResources', // Use the component name as the value
+      storedResources: [
+        {
+          id: 'official-page',
+          title: 'Official guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org/'
+        },
+        {
+          id: 'google-page',
+          title: 'Google guide',
+          description: 'The official Google JavaScript documentation',
+          link: 'https://google.com/'
+        }
+      ]
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'StoredResources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'AddingResources' ? null : 'flat';
+    }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'StoredResources';
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add any additional styling for your resources component here */
+</style>
